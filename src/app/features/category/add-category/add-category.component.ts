b0be4ee2fe47b2487184b955821dc7135636a097
pasindu-2002 <pasindualuthwalahewa@gmail.com>
2025-02-21 13:50:38 +0530

    feat: get category

--- src/app/features/category/add-category/add-category.component.ts
+++ src/app/features/category/add-category/add-category.component.ts
@@ -2,6 +2,7 @@ import { Component, OnDestroy } from '@angular/core';
 import { AddCategoryRequest } from '../models/add-category-request.model';
 import { CategoryService } from '../services/category.service';
 import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-category',
@@ -14,7 +15,9 @@ export class AddCategoryComponent implements OnDestroy {
   model: AddCategoryRequest;
   private addCategorySubscription?: Subscription;
 
-  constructor(private categoryService: CategoryService) {
+  constructor(private categoryService: CategoryService, 
+    private router: Router
+  ) {
     this.model = {
       name: '',
       urlHandle: ''
@@ -25,8 +28,7 @@ export class AddCategoryComponent implements OnDestroy {
       this.addCategorySubscription = this.categoryService.addCategory(this.model)
         .subscribe({
           next: (response) => {
-            console.log(response);
-            alert('Category added successfully');
+            this.router.navigate(['/admin/categories']);
           }
         })
   }
