83373e950cf122b06b8c70858d71bfea3554fb2f
pasindu-2002 <pasindualuthwalahewa@gmail.com>
2025-03-10 14:30:52 +0530

    feat: Store JWT Token & Update Auth Services

--- src/app/core/components/navbar/navbar.component.ts
+++ src/app/core/components/navbar/navbar.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/features/Auth/services/auth.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  constructor(private authServices: AuthService) {}
 
+  ngOnInit(): void {
+    this.authServices.user().subscribe({
+      next: (response) => {
+        console.log(response);
+      },
+    });
+  }
 }
