206254a811dd1a2aaa32fb221b35b26f3eb7332a
pasindu-2002 <pasindualuthwalahewa@gmail.com>
2025-03-12 20:11:28 +0530

    feat: filtering categories

--- src/app/features/category/services/category.service.ts
+++ src/app/features/category/services/category.service.ts
@@ -1,38 +1,56 @@
 import { Injectable } from '@angular/core';
 import { AddCategoryRequest } from '../models/add-category-request.model';
 import { Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Category } from '../models/category.model';
 import { environment } from 'src/environments/environment.development';
 import { UpdateCategoryRequest } from '../models/update-category-request.model';
 import { CookieService } from 'ngx-cookie-service';
+import { query } from '@angular/animations';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CategoryService {
+  constructor(private http: HttpClient, private cookieService: CookieService) {}
 
-  constructor(private http: HttpClient,
-    private cookieService: CookieService
-  ) { }
-
-  addCategory(model: AddCategoryRequest): Observable<void>{
-    return this.http.post<void>(`${environment.apiBaseUrl}/api/Category?addAuth=true`, model);
+  addCategory(model: AddCategoryRequest): Observable<void> {
+    return this.http.post<void>(
+      `${environment.apiBaseUrl}/api/Category?addAuth=true`,
+      model
+    );
   }
 
-  getAllCategories(): Observable<Category[]>{
-    return this.http.get<Category[]>(`${environment.apiBaseUrl}/api/Category`);
+  getAllCategories(query?: string): Observable<Category[]> {
+    let params = new HttpParams();
+
+    if (query) {
+      params = params.set('query', query);
+    }
+    return this.http.get<Category[]>(`${environment.apiBaseUrl}/api/Category`, {
+      params: params,
+    });
   }
 
-  getCategoryById(id: string): Observable<Category>{
-    return this.http.get<Category>(`${environment.apiBaseUrl}/api/Category/${id}`)
+  getCategoryById(id: string): Observable<Category> {
+    return this.http.get<Category>(
+      `${environment.apiBaseUrl}/api/Category/${id}`
+    );
   }
 
-  updateCategory(id: string, updateCategoryRequest: UpdateCategoryRequest): Observable<Category>{
-    return this.http.put<Category>(`${environment.apiBaseUrl}/api/Category/${id}?addAuth=true`, updateCategoryRequest);
+  updateCategory(
+    id: string,
+    updateCategoryRequest: UpdateCategoryRequest
+  ): Observable<Category> {
+    return this.http.put<Category>(
+      `${environment.apiBaseUrl}/api/Category/${id}?addAuth=true`,
+      updateCategoryRequest
+    );
   }
 
-  deleteCategory(id: string): Observable<Category>{
-    return this.http.delete<Category>(`${environment.apiBaseUrl}/api/Category/${id}?addAuth=true`);
+  deleteCategory(id: string): Observable<Category> {
+    return this.http.delete<Category>(
+      `${environment.apiBaseUrl}/api/Category/${id}?addAuth=true`
+    );
   }
 }
